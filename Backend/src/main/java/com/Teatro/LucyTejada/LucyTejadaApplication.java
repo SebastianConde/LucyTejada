5452796c4a399e59d3466eafda6321736e3dbcd4
SebastianConde <luissebastian.conde@utp.edu.co>
2025-03-27 12:46:14 -0500

    Primer commit: backend y frontend

---
+++ Backend/src/main/java/com/Teatro/LucyTejada/LucyTejadaApplication.java
@@ -0,0 +1,13 @@
+package com.Teatro.LucyTejada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LucyTejadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LucyTejadaApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/Teatro/LucyTejada/LucyTejadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Teatro.LucyTejada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LucyTejadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
