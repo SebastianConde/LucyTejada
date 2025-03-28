f60e0ac7486f4deddc3dd75748f8268a22eb8862
SebastianConde <luissebastian.conde@utp.edu.co>
2025-03-27 22:20:32 -0500

    Conexión a la BD real

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { LlamarBackendService } from './llamar-backend.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, HttpClientModule], // Importa los módulos necesarios
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Frontend';
+  data: any;
+
+  constructor(private backendService: LlamarBackendService) { }
+
+  ngOnInit() {
+    this.backendService.getData().subscribe(response => {
+      this.data = response;
+      console.log('Datos recibidos del backend:', this.data);
+    }, error => {
+      console.error('Error al obtener datos del backend:', error);
+    });
+  }
 }
