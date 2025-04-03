caa3a82b352c52aff12a305abad269379ce0e7bc
SebastianConde <luissebastian.conde@utp.edu.co>
2025-04-03 00:14:31 -0500

    Login con jwt y registro de usuarios

--- Backend/src/main/java/com/Teatro/LucyTejada/controller/LucyTejadaController.java
+++ Backend/src/main/java/com/Teatro/LucyTejada/controller/LucyTejadaController.java
@@ -8,6 +8,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import com.Teatro.LucyTejada.service.UsuarioService;
 import com.Teatro.LucyTejada.dto.RegistroRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.dao.DataIntegrityViolationException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -26,7 +28,13 @@ public class LucyTejadaController {
     @PostMapping("/lucyTejada/registrar")
     @PreAuthorize("hasRole('ROLE_Administrativo')")  // Se usa con "ROLE_"
     public ResponseEntity<String> registrarUsuario(@RequestBody RegistroRequest request) {
-        usuarioService.registrarUsuario(request);
-        return ResponseEntity.ok("Usuario registrado correctamente.");
+        try {
+            usuarioService.registrarUsuario(request);
+            return ResponseEntity.ok("Usuario registrado correctamente.");
+        } catch (DataIntegrityViolationException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Error: Datos duplicados o restricción de integridad.");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error interno del servidor: " + e.getMessage());
+        }
     }
 }
