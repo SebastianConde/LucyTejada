251406d063c865e29f3d17ffd512042f6a3411db
SebastianConde <luissebastian.conde@utp.edu.co>
2025-04-07 19:56:47 -0500

    Arreglado conexión endpoint back/front

--- Frontend/src/app/app.config.ts
+++ Frontend/src/app/app.config.ts
@@ -1,10 +1,11 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { NG_VALIDATORS } from '@angular/forms';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()), { provide: NG_VALIDATORS, useValue: [], multi: true }],
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()), { provide: NG_VALIDATORS, useValue: [], multi: true }, provideHttpClient(withFetch())],
 };
--- Frontend/src/app/services/login.service.ts
+++ Frontend/src/app/services/login.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { inject } from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
 export class LoginService {
-  constructor(private http: HttpClient) {}
+  private http = inject(HttpClient);
 
   login(data: { username: string; password: string }): Observable<any> {
     return this.http.post('http://localhost:8080/auth/login', data);
